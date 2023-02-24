539f754c185136e115f6ab8dcc402716f2ec4903
Mark <arashm35@gamesoultech.com>
2023-02-24 16:44:02 +0800

    update obsolete apis;

--- Editor/System/Importers/AudioImporterConfigurator.cs
+++ Editor/System/Importers/AudioImporterConfigurator.cs
@@ -1,60 +1,71 @@
 using UnityEditor;
-
 using System;
 using Model = UnityEngine.AssetGraph.DataModel.Version2;
 
 namespace UnityEngine.AssetGraph
 {
-    [Serializable] 
+    [Serializable]
     [CustomAssetImporterConfigurator(typeof(AudioImporter), "Audio", "setting.wav")]
     public class AudioImportSettingsConfigurator : IAssetImporterConfigurator
     {
-        public void Initialize (ConfigurationOption option)
+        public void Initialize(ConfigurationOption option)
         {
         }
 
-        public bool IsModified (AssetImporter referenceImporter, AssetImporter importer, BuildTarget target, string group)
-        {
-            var r = referenceImporter as AudioImporter;
-            var t = importer as AudioImporter;
-            if (r == null || t == null) {
-                throw new AssetGraphException ($"Invalid AssetImporter assigned for {importer.assetPath}");
-            }
-            return !IsEqual (t, r);
-        }
-
-        public void Configure (AssetImporter referenceImporter, AssetImporter importer, BuildTarget target, string group)
+        public bool IsModified(AssetImporter referenceImporter, AssetImporter importer, BuildTarget target,
+            string group)
         {
             var r = referenceImporter as AudioImporter;
             var t = importer as AudioImporter;
-            if (r == null || t == null) {
-                throw new AssetGraphException ($"Invalid AssetImporter assigned for {importer.assetPath}");
+            if (r == null || t == null)
+            {
+                throw new AssetGraphException($"Invalid AssetImporter assigned for {importer.assetPath}");
             }
-            OverwriteImportSettings (t, r);
+
+            return !IsEqual(t, r);
         }
 
-        public void OnInspectorGUI (AssetImporter referenceImporter, BuildTargetGroup target, Action onValueChanged)
+        public void Configure(AssetImporter referenceImporter, AssetImporter importer, BuildTarget target, string group)
+        {
+            var r = referenceImporter as AudioImporter;
+            var t = importer as AudioImporter;
+            if (r == null || t == null)
+            {
+                throw new AssetGraphException($"Invalid AssetImporter assigned for {importer.assetPath}");
+            }
+
+            OverwriteImportSettings(t, r);
+        }
+
+        public void OnInspectorGUI(AssetImporter referenceImporter, BuildTargetGroup target, Action onValueChanged)
         {
         }
 
-        private void OverwriteImportSettings (AudioImporter target, AudioImporter reference)
+        private void OverwriteImportSettings(AudioImporter target, AudioImporter reference)
         {
             target.defaultSampleSettings = reference.defaultSampleSettings;
             target.forceToMono = reference.forceToMono;
+#if !UNITY_2022_1_OR_NEWER
             target.preloadAudioData = reference.preloadAudioData;
+#endif
 
-            foreach (var g in NodeGUIUtility.SupportedBuildTargetGroups) {
-                var platformName = BuildTargetUtility.TargetToAssetBundlePlatformName (g,
-                                       BuildTargetUtility.PlatformNameType.AudioImporter);
+            foreach (var g in NodeGUIUtility.SupportedBuildTargetGroups)
+            {
+                var platformName = BuildTargetUtility.TargetToAssetBundlePlatformName(g,
+                    BuildTargetUtility.PlatformNameType.AudioImporter);
 
-                if (reference.ContainsSampleSettingsOverride (platformName)) {
-                    var setting = reference.GetOverrideSampleSettings (platformName);
-                    if (!target.SetOverrideSampleSettings (platformName, setting)) {
-                        LogUtility.Logger.LogError ("AudioImporter",
+                if (reference.ContainsSampleSettingsOverride(platformName))
+                {
+                    var setting = reference.GetOverrideSampleSettings(platformName);
+                    if (!target.SetOverrideSampleSettings(platformName, setting))
+                    {
+                        LogUtility.Logger.LogError("AudioImporter",
                             $"Failed to set override setting for {platformName}: {target.assetPath}");
                     }
-                } else {
-                    target.ClearSampleSettingOverride (platformName);
+                }
+                else
+                {
+                    target.ClearSampleSettingOverride(platformName);
                 }
             }
 
@@ -62,26 +73,32 @@ namespace UnityEngine.AssetGraph
             target.ambisonic = reference.ambisonic;
         }
 
-        private bool IsEqual (AudioImporter target, AudioImporter reference)
+        private bool IsEqual(AudioImporter target, AudioImporter reference)
         {
-            UnityEngine.Assertions.Assert.IsNotNull (reference);
+            UnityEngine.Assertions.Assert.IsNotNull(reference);
 
-            if (!IsEqualAudioSampleSetting (target.defaultSampleSettings, reference.defaultSampleSettings)) {
+            if (!IsEqualAudioSampleSetting(target.defaultSampleSettings, reference.defaultSampleSettings))
+            {
                 return false;
             }
 
-            foreach (var g in NodeGUIUtility.SupportedBuildTargetGroups) {
-                var platformName = BuildTargetUtility.TargetToAssetBundlePlatformName (g,
-                                       BuildTargetUtility.PlatformNameType.AudioImporter);
+            foreach (var g in NodeGUIUtility.SupportedBuildTargetGroups)
+            {
+                var platformName = BuildTargetUtility.TargetToAssetBundlePlatformName(g,
+                    BuildTargetUtility.PlatformNameType.AudioImporter);
 
-                if (target.ContainsSampleSettingsOverride (platformName) !=
-                    reference.ContainsSampleSettingsOverride (platformName)) {
+                if (target.ContainsSampleSettingsOverride(platformName) !=
+                    reference.ContainsSampleSettingsOverride(platformName))
+                {
                     return false;
                 }
-                if (target.ContainsSampleSettingsOverride (platformName)) {
-                    var t = target.GetOverrideSampleSettings (platformName);
-                    var r = reference.GetOverrideSampleSettings (platformName);
-                    if (!IsEqualAudioSampleSetting (t, r)) {
+
+                if (target.ContainsSampleSettingsOverride(platformName))
+                {
+                    var t = target.GetOverrideSampleSettings(platformName);
+                    var r = reference.GetOverrideSampleSettings(platformName);
+                    if (!IsEqualAudioSampleSetting(t, r))
+                    {
                         return false;
                     }
                 }
@@ -93,13 +110,16 @@ namespace UnityEngine.AssetGraph
                 return false;
             if (target.ambisonic != reference.ambisonic)
                 return false;
+#if !UNITY_2022_1_OR_NEWER
             if (target.preloadAudioData != reference.preloadAudioData)
                 return false;
+#endif
 
             return true;
         }
 
-        private bool IsEqualAudioSampleSetting (AudioImporterSampleSettings target, AudioImporterSampleSettings reference)
+        private bool IsEqualAudioSampleSetting(AudioImporterSampleSettings target,
+            AudioImporterSampleSettings reference)
         {
             // defaultSampleSettings
             if (target.compressionFormat != reference.compressionFormat)
